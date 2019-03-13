e91af5f390cb50849a1b094684175588d8c137bd
Veena Dali <veenadali@Veenas-MBP.hsd1.pa.comcast.net>
2019-03-13 08:10:42 -0400

    working through cleaned data

--- Analyses/2_Applicants/Veena/Exploration_vd.R
+++ Analyses/2_Applicants/Veena/Exploration_vd.R
@@ -6,17 +6,19 @@ mydata <- read.csv("Data/dog_apps.csv")
 
 hist(mydata[,6])
 
-#frequency of different types of allergies
-aggregate(data.frame(count = mydata$allergies), list(value = mydata[,9]), length)
-  #using dplyr
-  dplyr::count(mydata, allergies)
-  
+
+#aggregate(data.frame(count = mydata$allergies), list(value = mydata$allergies), length)
+
+#aggregate(data.frame(count = mydata$home_owner), list(value = mydata$home_owner), length)
+
+
+#Cleaned data created by Amy
+dataCleaned <- read.csv("Analyses/2_Applicants/amygood/output/apps_clean.csv")
+
 #frequency of different home_owner types
-aggregate(data.frame(count = mydata$home_owner), list(value = mydata$home_owner), length)
-  #using dplyr
-  dplyr::count(mydata, home_owner)
-
-
+dplyr::count(dataCleaned, home_owner)
+#frequency of different types of allergies
+dplyr::count(dataCleaned, allergies)
 
 
   
